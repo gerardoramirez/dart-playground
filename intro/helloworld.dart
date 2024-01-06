b49e691a6499cf23e6d489f4ec5b4993d48a577a
Gerardo Ramirez <133133190+ramirezzendrive@users.noreply.github.com>
2024-01-06 10:29:55 -0800

    working on integers and learning the way of the integer in Dart

--- intro/helloworld.dart
+++ intro/helloworld.dart
@@ -1,12 +1,13 @@
-
-class helloworld {
-  // example of one line comment, ignored! 
-
-
   //Execution of a Dart program always begins with a call to a function called main()
-  main() {
+  
+  /* A function consists of a header that gives its name and any parameters (our example has none) 
+   * followed by a body. The body of main() consists of a single statement, which is a call to another function, print() which takes a single argument. 
+   * The argument in this case is the string literal ‘Hola Mundo’. The effect is to print the words 
+   * “Hola Mundo”. 
+   */
+  void main() {
 
-    print('  Hola Mundo!');
+    print('Hola Mundo!');
 
     /* Here we declare twice with a parameter x. 
     * The function returns x multiplied by 2. 
@@ -14,12 +15,20 @@ class helloworld {
     twice(x) => x * 2;
     //We can invoke twice by writing
     print(twice(2));
+
+    integerExample();
   }
-   /* A function consists of a header that gives its name and any parameters (our example has none) 
-   * followed by a body. The body of main() consists of a single statement, which is a call to another function, print() which takes a single argument. 
-   * The argument in this case is the string literal ‘Hola Mundo’. The effect is to print the words 
-   * “Hola Mundo”. 
-   */
-
-} 
-
+/*
+ * Store a number without a decimal point.
+ */
+ void integerExample() {
+  /*
+   * In the Dart language, an integer uses the reference int. 
+   * In the preceding code example, the data type (e.g., int) is 
+   * the first part of the declaration. Next, a label is assigned 
+   * to the data type to be used, e.g., myVariable. Finally, we assign 
+   * a value to the data type—in this example, the value of 35.
+   */ 
+  int myVariable = 35;
+  print(myVariable);
+ }
