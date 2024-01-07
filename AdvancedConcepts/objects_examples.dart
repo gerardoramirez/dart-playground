ff388d1ce643066a11a9e2ea564b343006785ab4
Gerardo Ramirez <gerardoramirez@users.noreply.github.com>
2024-01-07 09:48:33 -0800

    Update objects_examples.dart
    Updating details of objects.

--- AdvancedConcepts/objects_examples.dart
+++ AdvancedConcepts/objects_examples.dart
@@ -1,1 +1,3 @@
-// Objects in dart
+/* Objects in Dart. It bears repeating: everything in Dart is an object. 
+ * This includes even the simplest data such as numbers or the Boolean values true and false.
+ */
