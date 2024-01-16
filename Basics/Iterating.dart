f3f5321f291d323ee1c9fd35da35aed842633101
Gerardo Ramirez <gerardoramirez@users.noreply.github.com>
2024-01-16 15:51:06 -0800

    Update Iterating.dart

--- Basics/Iterating.dart
+++ Basics/Iterating.dart
@@ -2,11 +2,26 @@
 
 
 void main() {
+  
+  regularWhileLoop();
+  exampleOfDoWhileLoop();
+}
 
+void regularWhileLoop() {
   bool isTrue = true;
   
   while (isTrue) {
     print ('Hello');
     isTrue = false;
   }
+}
+
+// Use a do while loop when you need the loop to be executed a minimum of one iteration.
+void exampleOfDoWhileLoop() {
+  bool isTrue = true;
+  
+  do {
+    print ('Hello');
+    isTrue = false;
+  } while (isTrue) ;
 }
