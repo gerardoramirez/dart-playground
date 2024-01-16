2c4e797c93b874522b89a47ae6284ac5bac1cca2
Gerardo Ramirez <gerardoramirez@users.noreply.github.com>
2024-01-16 15:41:37 -0800

    Create Iterating.dart

---
+++ Basics/Iterating.dart
@@ -0,0 +1,12 @@
+// Problem: You want a method to loop until a condition is satisfied within an application.
+
+
+void main() {
+
+  bool isTrue = true;
+  
+  while (isTrue) {
+    print ('Hello');
+    isTrue = false;
+  }
+}
