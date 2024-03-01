d908c6c10b4058f3bed5e194d7da40ca98111f61
Gerardo Ramirez <gerardo@muyal.io>
2024-03-01 07:52:41 -0800

    Update objects_examples.dart
    Creating point class as a demonstration.

--- AdvancedConcepts/objects_examples.dart
+++ AdvancedConcepts/objects_examples.dart
@@ -1,3 +1,25 @@
-/* Objects in Dart. It bears repeating: everything in Dart is an object. 
- * This includes even the simplest data such as numbers or the Boolean values true and false.
- */
+import 'dart:math';
+
+
+class Point {
+
+ var x, y;
+
+ Point(this.x, this.y);
+
+ scale(factor) => new Point(x * factor, y * factor);
+
+ operator +(p) => new Point(x + p.x, y + p.y);
+
+ static distance(p1, p2) {
+
+  var dx = p1.x - p2.x;
+
+  var dy = p1.y - p2.y;
+
+  return sqrt(dx * dx + dy * dy);
+
+ }
+
+}
+
